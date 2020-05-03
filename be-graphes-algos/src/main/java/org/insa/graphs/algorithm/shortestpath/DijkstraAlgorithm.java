ba97241c79ba9ce811bb3d77faa2762c744b6061
Gwen02 <62254832+Gwen02@users.noreply.github.com>
2020-05-03 16:15:47 +0200

    modif Dijkstra

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
@@ -1,4 +1,8 @@
-package org.insa.graphs.algorithm.shortestpath;
+import java.util.Iterator;
+
+import org.insa.algo.AbstractSolution.Status;
+import org.insa.algo.utils.*;
+import org.insa.graph.*;
 
 public class DijkstraAlgorithm extends ShortestPathAlgorithm {
 
@@ -8,9 +12,24 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
 
     @Override
     protected ShortestPathSolution doRun() {
+        //Initialisation du graphe
         final ShortestPathData data = getInputData();
+        Graph graph = data.getGraph();
+        BinaryHeap<Label> tas = new BinaryHeap<Label>();
+        
+        int nbSommets = graph.size(); //nombre de sommets du graphe
+        double[] distances = new double[nbSommets]; //Tableau de distances
+        Arrays.fill(distances, Double.POSITIVE_INFINITY); //Remplissage du tableau en +infini
+        distances[data.getOrigin().getId()] = 0; //Mise à 0 de l'origine du graphe
+        
+        notifyOriginProcessed(data.getOrigin()); //Notifie l'obsvervateur du premier élément
+        
+        Arc[] predecessorArcs = new Arc[nbSommets];
         ShortestPathSolution solution = null;
-        // TODO:
+        
+        while(solution == null) {
+        	
+        }
         return solution;
     }
 
